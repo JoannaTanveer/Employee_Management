6e5cfc684e57ce6758a548b21b78a15db4e377d2
JoannaTanveer <joanna.tanveer@gmail.com>
2020-08-25 22:15:35 -0700

    final

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -2,7 +2,7 @@
 const Employee = require("./employee.js");
 
 class Manager extends Employee {
-    constructor (name, id, email, school) {
+    constructor (name, id, email, officeNumber) {
         super(name, id, email, "Manager");
 
         this.officeNumber= officeNumber;
